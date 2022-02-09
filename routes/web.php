34115a6736cab7b2f6ff921f317ab323af3691a6
Nicola Taylor <t@ylornico.la>
2022-02-08 21:48:26 -0500

    Added basic login from Breeze. Started on post creation flow.

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/admin', function () {
+    return view('auth.dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
