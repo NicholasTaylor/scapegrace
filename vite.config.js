4b4b9af4425b1b9faac06cf9961a273e6ddf6131
Nicholas Taylor <t@ylornico.la>
2022-09-12 16:04:45 -0400

    Tweaked vite config to handle Valet via HTTPS more gracefully.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,33 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
+import {homedir} from 'os';
+import {resolve} from 'path';
+
+let host = 'scapegrace.test'
+
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
 
 export default defineConfig({
     plugins: [
@@ -15,4 +42,9 @@ export default defineConfig({
         reactRefresh(),
         react(),
     ],
+    /*server: {
+        https: true,
+        host: 'localhost',
+    }*/
+    server: detectServerConfig(host),
 });
